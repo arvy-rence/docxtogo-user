ecb6c1718daf988c1fd0589b7245b77bd89053dd
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2023-01-12 21:50:50 +0800

    [REV 01/12/23] / Changes from revision list 01/11/23

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Footer} from "flowbite-react";
-import {FooterDivider} from "flowbite-react/lib/esm/components/Footer/FooterDivider";
-
 
 const CustomFooter = () => {
   return (
